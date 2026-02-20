ce2dce8116770745f505e3d0e3e20f7d5cbdc639
Jaydeep097 <jaydeepyaduvanshi09@gmail.com>
2026-02-21 00:05:07 +0530

    Update API calling scripts and HTML

--- tailwind/API_Calling_js/script.js
+++ tailwind/API_Calling_js/script.js
@@ -1,6 +1,11 @@
 const button = document.getElementById("search-button")
 const input = document.getElementById("city-input")
 
+const cityName = document.getElementById("city-name")
+const cityTime = document.getElementById("city-time")
+const cityTemp = document.getElementById("city-temp")
+const cityWind = document.getElementById("city-wind")
+
 async function getData(cityName){
     const promise = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=8ced1c6438fa47d685c195903261902&q=${cityName}&aqi=yes`  
@@ -12,7 +17,10 @@ async function getData(cityName){
 button.addEventListener('click', async()=>{
     const value = input.value;
     const result = await getData(value)
-    console.log(result)
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`
+    cityTime.innerText = `${result.current.localtime}`
+    cityTemp.innerText = `${result.current.temp_c} C`
+    cityWind.innerText = `${result.current.wind_kph} km/h`
 })
 
 
